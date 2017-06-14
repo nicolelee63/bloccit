8473c204a7aec6e95a3e8cf10d8a14bb3dadac74
Nicole Lee <nicole.lee633@gmail.com>
2017-06-14 19:49:53 +0800

    checkpoint 14 completed

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,12 @@
 class WelcomeController < ApplicationController
-  def index
-  end
+    
+    def index
+    end
 
-  def about
-  end
+    def about
+    end
+    
+    def faq
+    end
+    
 end
