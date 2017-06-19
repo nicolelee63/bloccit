db2e783de7e5a162ad9282eef1ba82ee1feb1f5f
Nicole Lee <nicole.lee633@gmail.com>
2017-06-19 17:36:11 +0800

    checkpoint 22 completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,19 @@
 require 'random_data'
 
+
+15.times do
+	Topic.create!(
+    name:         RandomData.random_sentence,
+    description:  RandomData.random_paragraph
+   )
+end
+topics = Topic.all
+
 # Create Posts
 50.times do
 
     Post.create!(
-
+    topic: topics.smaple,
     title:  RandomData.random_sentence,
     body:   RandomData.random_paragraph
 )
@@ -22,5 +31,6 @@ posts = Post.all
 end
  
 puts "Seed finished"
+puts "#{Topic.count} topics created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
