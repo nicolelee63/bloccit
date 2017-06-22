3a2de4ab296ea5148a77620a68b3622ea4229ab4
nicolelee63 <nicole.lee633@gmail.com>
2017-06-22 13:13:33 +0800

    Assignent 25 completed

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,10 @@ Rails.application.routes.draw do
 	end
 
 	resources :users, only: [:new, :create]
+	post 'users/confirm' => 'users#confirm'
 
   get 'about' => 'welcome#about'
 
   root 'welcome#index'
+  
 end
