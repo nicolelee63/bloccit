aea62f15dfb0e178ad6911fa2c3efc48eedf52fb
nicolelee63 <nicole.lee633@gmail.com>
2017-06-22 14:49:13 +0800

    Checkpoint 27 completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,14 @@
 require 'random_data'
 
+5.times do
+    User.create!(
+        name: RandomData.random_name,
+        email: RandomData.random_email,
+        password: RandomData.random_sentence
+    )
+end
+
+users = User.all
 
 15.times do
 	Topic.create!(
@@ -13,9 +22,10 @@ topics = Topic.all
 50.times do
 
     Post.create!(
-    topic: topics.smaple,
-    title:  RandomData.random_sentence,
-    body:   RandomData.random_paragraph
+        user: users.sample,
+        topic: topics.sample,
+        title:  RandomData.random_sentence,
+        body:   RandomData.random_paragraph
 )
 end
 posts = Post.all
@@ -29,8 +39,15 @@ posts = Post.all
     body: RandomData.random_paragraph
 )
 end
+
+user = User.first
+ user.update_attributes!(
+   email: 'youremail.com', # replace this with your personal email
+   password: 'helloworld'
+)
  
 puts "Seed finished"
+puts "#{User.count} users created"
 puts "#{Topic.count} topics created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
