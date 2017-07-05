1197e77b71d736e44883359ea42b91775115858a
nicolelee63 <nicole.lee633@gmail.com>
2017-07-05 21:52:00 +0800

    assignment 31 completed

--- app/mailers/favorite_mailer.rb
+++ app/mailers/favorite_mailer.rb
@@ -1,14 +1,24 @@
 class FavoriteMailer < ApplicationMailer
 	default from: "nicole.lee633@gmail.com"
 
-	def new_comment(user, post, comment)
+    def new_comment(user, post, comment)
 		headers["Message-ID"] = "<comments/#{comment.id}@your-app-name.example>"
-    headers["In-Reply-To"] = "<post/#{post.id}@your-app-name.example>"
-    headers["References"] = "<post/#{post.id}@your-app-name.example>"
+        headers["In-Reply-To"] = "<post/#{post.id}@your-app-name.example>"
+        headers["References"] = "<post/#{post.id}@your-app-name.example>"
  
-    @user = user
-    @post = post
-    @comment = comment
-    mail(to: user.email, subject: "New comment on #{post.title}")
-  end
+        @user = user
+        @post = post
+        @comment = comment
+        mail(to: user.email, subject: "New comment on #{post.title}")
+    end
+
+    def new_post(post)
+        headers["Message-ID"] = "<posts/#{post.id}@your-app-name.example>"
+        headers["In-Reply-To"] = "<post/#{post.id}@your-app-name.example>"
+        headers["References"] = "<post/#{post.id}@your-app-name.example>"
+
+        @post = post
+
+        mail(to: post.user.email, subject: "You're following, #{post.title}!")
+    end
 end
